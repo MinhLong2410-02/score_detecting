924c4086f806334102498fd41e87a29ada209665
MinhLong2410-02 <minhlong2002@gmail.com>
2022-10-18 15:46:23 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import json
+from my_func import *
+
+width, height = 700, 900
+
+img = cv.imread("Img/01.png")
+img = cv.resize(img, (width, height))
+# show_images(img[295:535, 130:270])
+sbd_img = img_preprocessing(img[295:535, 130:270])
+
+data = {}
+data['data'] = {}
+data['data']['sbd'] = get_mark_sbd(sbd_img)
+
+
+data['data']['answer'] = get_score(img)
+print(data)
+data = json.dumps(data)
+with open('data.json', "w") as f:
+    json.dump(data, f, indent = 4)
